38edc9ffb438bc7f9f336c79c45f5db937a6ff31
Leonardo Frid <leonardo.frid@apexrms.com>
2020-05-07 14:33:50 -0700

    updated header block

--- src/deploy.R
+++ src/deploy.R
@@ -1,8 +1,8 @@
 # Shiny application for SyncroSim epidemic package COVID-19 output.
-# Copyright ? 2007-2020 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
+# Copyright 2007-2020 Apex Resource Management Solutions Ltd. (ApexRMS). All rights reserved.
 # The TERMS OF USE and END USER LICENSE AGREEMENT for this software can be found in the LICENSE file.
 
-# This script copies the most up-to-date model outputs from the "data" folder to the "shiny/covid19canada/data" folder
+# This script copies the most up-to-date model outputs from the "data" folder in the covid19sim repository to the "src/covid19canada/data" folder for this repository
 
 # Set the working directory to the script's folder (works only in RStudio)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
